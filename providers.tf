8bbf5abd47497afd60fd62c5f30f5f42da1f6ae7
Dana Ivanov <82861779+divnv22@users.noreply.github.com>
2026-09-18 13:17:58 +0300

    Configure Azure remote backend for Terraform state

--- providers.tf
+++ providers.tf
@@ -4,7 +4,14 @@ terraform {
       source = "hashicorp/azurerm"
     }
   }
+  backend "azurerm" {
+    resource_group_name   = "rg-terraform-state-swc-01"
+    storage_account_name  = "stnutritiontfstateswc01"
+    container_name        = "tfstate"
+    key                   = "nutrition.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   features {}
